6036c333b9b7cf988920dbb7cb78019de28821e3
Arin Bryan <azb1g24@soton.ac.uk>
2026-03-14 19:59:33 +0000

    added conversation finalisation

--- nextjs-web-frontend/sotonhack2026/src/lib/actions/conversationActions.ts
+++ nextjs-web-frontend/sotonhack2026/src/lib/actions/conversationActions.ts
@@ -1,24 +1,53 @@
 "use server"
 import { generateTopicStrings } from "@/lib/actions/geminiAction";
 import { Conversation, User, getUserById } from "@/lib/actions/dbActions";
+import { transcribeAudioBlob } from "@/lib/actions/transcribeAction";
+import { insertConversation } from "@/lib/actions/dbActions";
 import type { SpeechToTextChunkResponseModel } from "@elevenlabs/elevenlabs-js/api";
 
-async function responseToConversation(response: SpeechToTextChunkResponseModel, pariticipants: {"name": string, "id": string}[]) {
+export async function responseToConversation(response: SpeechToTextChunkResponseModel, participants: {"name": string, "id": string}[]) : Promise<Omit<Conversation, "id">|null> {
     if (response.words.length < 1) { return null; }
     
-    let users = await Promise.all(pariticipants.map((p) => p.id).map(getUserById));
+    let users = await Promise.all(participants.map((p) => p.id).map(getUserById));
+
+    if (users == null) { return null; }
+
     let vocab = response.text.split(" ");
-    let topics = generateTopicStrings(response.text);
+    let topics = await generateTopicStrings(response.text);
+    if (topics.error != null) { return null; }
+
+    let this_convo_vocab = new Set<string>(vocab);
+    let participant1_vocab = new Set<string>((await getUserById(participants[0].id))?.vocab);
+    let participant2_vocab = new Set<string>((await getUserById(participants[1].id))?.vocab);
+    let participant1_new_vocab = this_convo_vocab.difference(participant1_vocab);
+    let participant2_new_vocab = this_convo_vocab.difference(participant2_vocab);
+
     let dateStamp = new Date();
     let length = response.words.at(-1)?.end;
 
     if (length === undefined) { return null; }
     
     return {
-        "users" : users,
-        "vocab" : vocab,
-        "topics": topics,
-        "dateStamp" : dateStamp,
-        "length" : length
+        vocab : vocab,
+        new_vocab: [Array.from(participant1_new_vocab), Array.from(participant2_new_vocab)],
+        participants: participants,
+        topics: topics.topics,
+        dateStamp : dateStamp,
+        length: length,
     }
+}
+
+async function finaliseConversation(recording: Blob, participants: {"name":string,"id":string}[]) {
+    if (!["audio/wav", "audio/mp3", "audio/mpeg", "audio/ogg", "audio/webm"].includes(recording.type)) {
+        return Promise.reject(new Error("Blob did not contain audio data"));
+    }
+
+    let transcribeResult = await transcribeAudioBlob(recording);
+    if (transcribeResult.error) { Promise.reject(new Error("Transcription Error")); }
+
+    let new_conversation = await responseToConversation(transcribeResult.apiresponse as SpeechToTextChunkResponseModel, participants);
+    if (new_conversation == null) { return Promise.reject(new Error("Could not generate conversation")); }
+
+    let pushed_convo = insertConversation(new_conversation);
+    if (pushed_convo === null) { Promise.reject(new Error("Could not insert conversation into database")); }
 }
